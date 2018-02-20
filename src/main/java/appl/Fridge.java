55187144d1cddda5b78755415faabbb38b2e5a39
dcodeh <dcodyburrows@gmail.com>
2018-02-19 20:35:18 -0500

    The fridge is mostly empty...
    I should work on other things first.

--- src/main/java/appl/Fridge.java
+++ src/main/java/appl/Fridge.java
@@ -4,10 +4,26 @@ import java.util.HashSet;
 
 import model.Item;
 
+/**
+ * Stores and tracks all of the shared and unshared items.
+ * @author dcodeh
+ */
 public class Fridge {
 
     // huh it's empty
     
-    private HashSet<Item> items;
+    private HashSet<Item> fridge;
+
+    public Fridge() {
+        fridge = new HashSet<Item>();
+    }
+    
+    public void addItem(Item i) {
+        fridge.add(i);
+    }
+    
+    public void removeItem(Item i) {
+        fridge.remove(i);
+    }
     
 }
