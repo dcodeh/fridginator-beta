cf2221ac2c1caac460df683e08f9434bf9f20c71
dcodeh <dcodyburrows@gmail.com>
2018-05-02 21:07:37 -0400

    That was fun
    I thought I would pound out the framework to make data persistent before
    going to bed tonight...then I realized I have no idea how this should
    work yet...oh well

---
+++ src/main/java/model/Csverializable.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * This abstract class helps other classes write themselves out to 
+ * a CSV file.
+ * @author dcodeh
+ */
+public abstract class Csverializable {
+
+	// TODO figure out how it works
+	
+}
